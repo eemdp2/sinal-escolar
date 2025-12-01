7c72e8b42a2dc9eaeb4124c49726aab813b2915b
escolamilitar <leonardo.portal@edu.mt.gov.br>
2025-12-01 08:46:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,30 @@
-const CACHE_NAME = 'sinal-escolar-v1';
+const CACHE_NAME = 'sinal-escolar-v3'; // Mudei a versão para forçar atualização
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
   './manifest.json',
-  './sons/sinal1.mp3' 
-  // Adicione todos os seus MP3s aqui
+  './icon-192.png',
+  './icon-512.png',
+  // Lista de todos os sons
+  './sons/a1.mp3',
+  './sons/a2.mp3',
+  './sons/a3.mp3',
+  './sons/a4.mp3',
+  './sons/a5.mp3',
+  './sons/a6.mp3',
+  './sons/a7.mp3',
+  './sons/a8.mp3',
+  './sons/a9.mp3',
+  './sons/fim.mp3'
 ];
 
-// Instalação: Cacheia os arquivos
+// O resto do código continua igual...
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Busca: Serve arquivos do cache se estiver offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
